8de74cf021c8acb75c62966f4ca69011a2d31cbd
Maksim Shelest <shelest.ks@gmail.com>
2023-03-10 16:47:17 +0300

    dropdown types

--- src/components/CallsContainer/CallsContainer.tsx
+++ src/components/CallsContainer/CallsContainer.tsx
@@ -9,6 +9,7 @@ import DatepickerComponent from '../DatepickerComponent/DatepickerComponent';
 import { IDatepickerComponentListItem } from '../../models/IDatepicker';
 import { useCalls } from '../../hooks/useCalls';
 import IconAdd from '../UI/Icons/IconAdd';
+import DropDown from '../Dropdown/DropDown';
 
 const CallsContainer = () => {
   const datePikerListItems: IDatepickerComponentListItem[] = useMemo(() => {
@@ -19,6 +20,45 @@ const CallsContainer = () => {
       { value: 364, name: 'Год' },
     ];
   }, []);
+  const typesListItems: IDatepickerComponentListItem[] = useMemo(() => {
+    return [
+      { value: 0, name: 'Исходящие' },
+      { value: 1, name: 'Входищие' },
+    ];
+  }, []);
+  const callsListItems: IDatepickerComponentListItem[] = useMemo(() => {
+    return [
+      { value: 2, name: '3 дня' },
+      { value: 6, name: 'Неделя' },
+      { value: 30, name: 'Месяц' },
+      { value: 364, name: 'Год' },
+    ];
+  }, []);
+  const sourcesListItems: IDatepickerComponentListItem[] = useMemo(() => {
+    return [
+      { value: 2, name: '3 дня' },
+      { value: 6, name: 'Неделя' },
+      { value: 30, name: 'Месяц' },
+      { value: 364, name: 'Год' },
+    ];
+  }, []);
+  const ratingListItems: IDatepickerComponentListItem[] = useMemo(() => {
+    return [
+      { value: 2, name: '3 дня' },
+      { value: 6, name: 'Неделя' },
+      { value: 30, name: 'Месяц' },
+      { value: 364, name: 'Год' },
+    ];
+  }, []);
+  const errorsListItems: IDatepickerComponentListItem[] = useMemo(() => {
+    return [
+      { value: 2, name: '3 дня' },
+      { value: 6, name: 'Неделя' },
+      { value: 30, name: 'Месяц' },
+      { value: 364, name: 'Год' },
+    ];
+  }, []);
+
   const [fetchCalls, calls, isLoading, error] = useAxios<ICallList>(
     {
       url: process.env.REACT_APP_URL,
@@ -89,7 +129,10 @@ const CallsContainer = () => {
           <div className={classes.balance}>
             <span className={classes['balance-text']}>Баланс:</span>
             <span className={classes['balance-number']}>272 ₽</span>
-            <IconAdd color="#005FF8" size='24px'/>
+            <IconAdd
+              color="#005FF8"
+              size="24px"
+            />
           </div>
           {!isLoading && !error && (
             <DatepickerComponent
@@ -105,6 +148,15 @@ const CallsContainer = () => {
               onArrowClickHandler={onArrowClickHandler}
             />
           )}
+        </div>
+        <div className={classes['filers-block']}>
+          <div className={classes.search}></div>
+          <div className={classes.filters}>
+            <DropDown
+              items={typesListItems}
+              initalValue={'Все типы'}
+            />
+          </div>
         </div>
       </div>
       <div className={classes['table-container']}>
--- src/components/Dropdown/DropDown.tsx
+++ src/components/Dropdown/DropDown.tsx
@@ -1,39 +1,66 @@
+import { useEffect, useRef, useState } from 'react';
 import classes from './DropDown.module.scss';
 
-interface ListItemProps {
-  text: string;
-  img?: string;
+interface DropDownProps<T> {
+  items: T[];
+  initalValue: string;
 }
 
-interface DropDownProps {
-  items: ListItemProps[];
-}
+const Dropdown = <T extends { value?: number; name?: string; img?: string }>({
+  items,
+  initalValue,
+}: React.PropsWithChildren<DropDownProps<T>>): JSX.Element => {
+  const [dropdownVisible, setDropdownIsVisible] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-const DropDown: React.FC<DropDownProps> = ({ items }): JSX.Element => {
-  const DropDownItem: React.FC<ListItemProps> = (props): JSX.Element => {
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (dropdownRef.current && !e.composedPath().includes(dropdownRef.current)) {
+        setDropdownIsVisible(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  const DropDownItem: React.FC<T> = (props): JSX.Element => {
     return (
       <li className={classes.item}>
-        <div>
-          <img
-            src={props.img}
-            alt="item"
-          />
-        </div>
-        <span>{props.text}</span>
+        {props.img && (
+          <div>
+            <img
+              src={props.img}
+              alt="item"
+            />
+          </div>
+        )}
+        <span>{props.name}</span>
       </li>
     );
   };
   return (
     <div className={classes.container}>
-      <ul>
-        {items.map((item: ListItemProps, index) => (
-          <DropDownItem
-            text={item.text}
-            key={index}
-          />
-        ))}
-      </ul>
+      <div
+        ref={dropdownRef}
+        onClick={() => setDropdownIsVisible((prevVisible) => !prevVisible)}
+      >
+        {initalValue}
+      </div>
+      {dropdownVisible && (
+        <div className={classes.menu}>
+          <ul>
+            {items.map((item, index) => (
+              <DropDownItem
+                {...item}
+                key={index}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
-export default DropDown;
+export default Dropdown;

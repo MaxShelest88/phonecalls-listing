b73f8a1266b3bda1dca3857342a335485d2cd3c8
Maksim Shelest <shelest.ks@gmail.com>
2023-03-15 14:51:44 +0300

    fix adiocontext

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useEffect, useRef, useState } from 'react';
+import IconPlay from '../UI/Icons/IconPlay';
 import classes from './Player.module.scss';
 
 interface PlayerProps {
@@ -8,25 +9,35 @@ interface PlayerProps {
 }
 
 const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element => {
-  const ctx = new AudioContext();
-  let audio: AudioBuffer;
-  const sound = ctx.createBufferSource();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+  const audioCtxContainer = useRef(null);
+  audioCtxContainer.current = new AudioContext();
+  const audioRef = useRef();
   const URL = process.env.REACT_APP_URL;
   const TOKEN = process.env.REACT_APP_TOKEN;
 
   const fetchAudio = async () => {
-    const { data } = await axios('getRecord', {
-      baseURL: URL,
-      method: 'POST',
-      params: {
-        record: 'MToxMDA2NzYxNToxNDMwMDM3NzExNzow',
-        partnership_id: '578',
-      },
-      headers: { Authorization: `Bearer ${TOKEN}` },
-      responseType: 'arraybuffer',
-    });
-    const decodedAudio = await ctx.decodeAudioData(data);
-    audio = decodedAudio;
+    setIsLoading(true);
+    try {
+      const { data } = await axios('getRecord', {
+        baseURL: URL,
+        method: 'POST',
+        params: {
+          record: 'MToxMDA2NzYxNToxNDMwMDM3NzExNzow',
+          partnership_id: '578',
+        },
+        headers: { Authorization: `Bearer ${TOKEN}` },
+        responseType: 'arraybuffer',
+      });
+      const decodedAudio = await audioCtxContainer.current.decodeAudioData(data);
+      audioRef.current = decodedAudio;
+      setIsLoading(false);
+    } catch (AxiosError) {
+      const error = AxiosError as AxiosError;
+      setError(error.message);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -34,14 +45,30 @@ const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element 
   }, []);
 
   const onPlay = () => {
-    sound.buffer = audio;
-    sound.connect(ctx.destination);
-    sound.start(ctx.currentTime);
+    const source = audioCtxContainer.current.createBufferSource();
+    source.buffer = audioRef.current;
+    source.connect(audioCtxContainer.current.destination);
+    source.start(audioCtxContainer.current.currentTime);
   };
+
+  //   const onStop = () => {
+  //     source.buffer = audio;
+  //     source.connect(ctx.destination);
+  //     source.stop(ctx.currentTime);
+  //   };
 
   return (
     <div className={classes.player}>
-      <button onClick={onPlay}>Play</button>
+      <div className={classes.time}></div>
+      <button
+        className={classes['btn-play']}
+        onClick={onPlay}
+      >
+        <IconPlay color="#002CFB" />
+      </button>
+      <div className={classes['progress-container']}>
+        <div className={classes.progress} />
+      </div>
     </div>
   );
 };
---
+++ src/components/UI/Icons/IconPause.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IconComponentsProps } from '../../../models/IIcon';
+
+const IconPause: React.FC<IconComponentsProps> = ({ color, size }) => (
+  <svg
+    width="8"
+    height="8"
+    viewBox="0 0 8 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0 8H2.66667V0H0V8ZM5.33333 0V8H8V0H5.33333Z"
+      fill={color}
+    />
+  </svg>
+);
+
+export default IconPause;

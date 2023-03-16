a423680b13063e3040b09ba1bf59f60d19c5efdf
Maksim Shelest <shelest.ks@gmail.com>
2023-03-16 13:10:12 +0300

    types styles

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -11,16 +11,16 @@ interface PlayerProps {
 }
 
 const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const [startedAt, setStartedAt] = useState(0);
-  const [stoppedAt, setStoppedAt] = useState(0);
-  const [rate, setRate] = useState(0);
+  const [startedAt, setStartedAt] = useState<number>(0);
+  const [stoppedAt, setStoppedAt] = useState<number>(0);
+  const [rate, setRate] = useState<number>(0);
   const [play, setPlay] = useState(false);
   const audioCtxContainer = useRef<AudioContext | undefined>();
-  const audioRef = useRef<AudioBuffer>();
+  const audioRef = useRef<AudioBuffer | undefined>();
   audioCtxContainer.current = new AudioContext();
-  const sourceRef = useRef<AudioBufferSourceNode>();
+  const sourceRef = useRef<AudioBufferSourceNode | undefined>();
 
   const URL = process.env.REACT_APP_URL;
   const TOKEN = process.env.REACT_APP_TOKEN;
@@ -52,7 +52,6 @@ const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element 
     fetchAudio();
   }, []);
 
-
   useEffect(() => {
     const interval = setInterval(() => {
       const duration = audioRef.current?.duration;
@@ -60,11 +59,11 @@ const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element 
         const playbackTime = (Date.now() - startedAt) / 1000;
         const progressPercent = Math.round((playbackTime * 100) / duration);
         setRate(progressPercent);
-        if (playbackTime >= duration) {
+        if (playbackTime > duration) {
           setRate(0);
           clearInterval(interval);
           setPlay(false);
-          sourceRef.current!.disconnect();
+          sourceRef.current?.disconnect();
         }
       }
     }, 1000);
@@ -76,20 +75,20 @@ const Player: React.FC<PlayerProps> = ({ record, partnership_id }): JSX.Element 
       audioCtxContainer.current.resume();
     }
     sourceRef.current = audioCtxContainer?.current?.createBufferSource();
-    if (sourceRef.current) {
+    if (sourceRef.current && audioCtxContainer.current) {
       sourceRef.current.buffer = audioRef.current as AudioBuffer;
-      sourceRef.current.connect(audioCtxContainer?.current!.destination);
+      sourceRef.current.connect(audioCtxContainer.current.destination);
       sourceRef.current.start(0, stoppedAt / 1000);
       setPlay(true);
       setStartedAt(Date.now() - stoppedAt);
     }
   }, [stoppedAt]);
 
-  const onStop = () => {
-    sourceRef.current!.stop();
+  const onStop = useCallback(() => {
+    sourceRef.current?.stop();
     setPlay(false);
     setStoppedAt(Date.now() - startedAt);
-  };
+  }, [startedAt]);
 
   if (error) {
     return <div>Ошибка загрузки аудио: {error}</div>;

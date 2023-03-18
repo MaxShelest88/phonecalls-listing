47a2735ff3cf02369a8421472665cd87e6f4f24c
Maksim Shelest <shelest.ks@gmai.com>
2023-03-18 14:21:37 +0300

    Loader

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,10 +1,11 @@
 import CallsContainer from '../CallsContainer/CallsContainer';
+import Loading from '../Loading/Loading';
 
 const Content: React.FC = () => {
   return (
     <div className="content">
       <div className="container">
-        <CallsContainer />
+			  <CallsContainer />
       </div>
     </div>
   );
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,6 +1,11 @@
 import classes from './Loading.module.scss';
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className={classes['spinner-container']}>
+      <div className={classes.spinner} />
+      <span className={classes.text}>Загружаем</span>
+    </div>
+  );
 };
 export default Loading;

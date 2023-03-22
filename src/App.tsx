970e3c25d36be02c71486e725050c375669d41a1
Maksim Shelest <shelest.ks@gmail.com>
2023-03-22 14:39:46 +0300

    refactor imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Content from 'components/Content/Content';
+import Header from 'components/Header/Header';
+import Sidebar from 'components/Sidebar/Sidebar';
 import './App.scss';
-import Content from './components/Content/Content';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
